a043a99b3e38271474acc236ac3d0304fd951925
MengxuanHUANG <53021701+MengxuanHUANG@users.noreply.github.com>
2023-03-24 16:04:45 -0400

    add subdivision control for BSplineSurface

--- src/MayaNodes/BSplineSurfaceNode.cpp
+++ src/MayaNodes/BSplineSurfaceNode.cpp
@@ -81,6 +81,18 @@ MStatus BSplineSurfaceNode::initialize()
     MAKE_INPUT(nAttr)
     addAttribute(BSplineSurfaceNode::inCPSH);
 
+    // input attributes
+    BSplineSurfaceNode::inSW = nAttr.create("SubdivisionWidth", "sw", MFnNumericData::kInt, 15);
+    MAKE_INPUT(nAttr)
+    addAttribute(BSplineSurfaceNode::inSW);
+
+    // input attributes
+    BSplineSurfaceNode::inSH = nAttr.create("SubdivisionHeight", "sh", MFnNumericData::kInt, 15);
+    MAKE_INPUT(nAttr)
+    addAttribute(BSplineSurfaceNode::inSH);
+
+    returnStatus = attributeAffects(BSplineSurfaceNode::inSW, BSplineSurfaceNode::outSurface);
+    returnStatus = attributeAffects(BSplineSurfaceNode::inSH, BSplineSurfaceNode::outSurface);
     returnStatus = attributeAffects(BSplineSurfaceNode::controlPlane, BSplineSurfaceNode::outSurface);
     return returnStatus;
 }
@@ -91,10 +103,16 @@ MStatus BSplineSurfaceNode::compute(const MPlug& plug, MDataBlock& data)
     if (plug == BSplineSurfaceNode::outSurface) {
 
         /* Get controlPlane's subdivision width & height */
-        MDataHandle swData = data.inputValue(BSplineSurfaceNode::inCPSW, &returnStatus);
-        int cpsw = swData.asInt();
-        MDataHandle shData = data.inputValue(BSplineSurfaceNode::inCPSH, &returnStatus);
-        int cpsh = shData.asInt();
+        MDataHandle cpswData = data.inputValue(BSplineSurfaceNode::inCPSW, &returnStatus);
+        int cpsw = cpswData.asInt();
+        MDataHandle cpshData = data.inputValue(BSplineSurfaceNode::inCPSH, &returnStatus);
+        int cpsh = cpshData.asInt();
+
+        // Get subdivision parameters for BSplaneSurface 
+        MDataHandle swData = data.inputValue(BSplineSurfaceNode::inSW, &returnStatus);
+        int sw = swData.asInt();
+        MDataHandle shData = data.inputValue(BSplineSurfaceNode::inSH, &returnStatus);
+        int sh = shData.asInt();
 
         MDataHandle controlPlaneData = data.inputValue(BSplineSurfaceNode::controlPlane, &returnStatus);
         MObject controlPlane = controlPlaneData.asMesh();
@@ -110,7 +128,7 @@ MStatus BSplineSurfaceNode::compute(const MPlug& plug, MDataBlock& data)
         MObject newOutputData = dataCreator.create(&returnStatus);
 
         // make new Mesh for output BSPlineSurface surface
-        this->MakeMesh(controlPoints, newOutputData, returnStatus);
+        this->MakeMesh(controlPoints, sw, sh, newOutputData, returnStatus);
 
         MDataHandle outputHandle = data.outputValue(BSplineSurfaceNode::outSurface, &returnStatus);
         outputHandle.set(newOutputData);
@@ -121,7 +139,11 @@ MStatus BSplineSurfaceNode::compute(const MPlug& plug, MDataBlock& data)
     return MS::kSuccess;
 }
 
-void BSplineSurfaceNode::MakeMesh(std::vector<std::vector<glm::vec3>>& controlPoints, MObject& meshData, MStatus& status)
+void BSplineSurfaceNode::MakeMesh(std::vector<std::vector<glm::vec3>>& controlPoints,
+                                  int subdivisionWidth,
+                                  int subdivisionHeight,
+                                  MObject& meshData,
+                                  MStatus& status)
 {
     // construct BSpline Surface
     BsplineSurface bs00{ 3, 3 };
@@ -137,27 +159,29 @@ void BSplineSurfaceNode::MakeMesh(std::vector<std::vector<glm::vec3>>& controlPo
     MIntArray faceCounts;
     MIntArray faceConnects;
 
-    float size = 15.f;
     glm::vec3 pt;
-
-    for (int u = 0; u <= static_cast<int>(size); ++u)
+    int size = 15;
+    for (int u = 0; u <= subdivisionWidth; ++u)
     {
-        for (int v = 0; v <= static_cast<int>(size); ++v)
+        for (int v = 0; v <= subdivisionHeight; ++v)
         {
-            bs00.surfacePoint(static_cast<float>(u) / size, static_cast<float>(v) / size, pt);
+            bs00.surfacePoint(static_cast<float>(u) / static_cast<float>(subdivisionWidth),
+                              static_cast<float>(v) / static_cast<float>(subdivisionHeight),
+                              pt);
+
             points.append(MPoint(pt.x, pt.y, pt.z));
         }
     }
 
     // make points, faceCounts, and face Connects
-    for (int w = 1; w <= size; ++w) // totoally size * size faces
+    for (int w = 1; w <= subdivisionWidth; ++w) // totoally size * size faces
     {
-        for (int h = 1; h <= size; ++h)
+        for (int h = 1; h <= subdivisionHeight; ++h)
         {
-            faceConnects.append(Index(size + 1, w - 1, h - 1));
-            faceConnects.append(Index(size + 1, w - 1, h));
-            faceConnects.append(Index(size + 1, w, h));
-            faceConnects.append(Index(size + 1, w, h - 1));
+            faceConnects.append(Index(subdivisionHeight + 1, w - 1, h - 1));
+            faceConnects.append(Index(subdivisionHeight + 1, w - 1, h));
+            faceConnects.append(Index(subdivisionHeight + 1, w, h));
+            faceConnects.append(Index(subdivisionHeight + 1, w, h - 1));
             faceCounts.append(4);
         }
     }
